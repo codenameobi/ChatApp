a04089c66609de86257f1d63c3768b36d61b4a62
admin <tobynwude@gmail.com>
2022-03-25 08:31:36 +0100

    can update chat in the database

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -6,11 +6,21 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var chatRecyclerView: RecyclerView
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
+
+    //use to create a unique private room for the receiver and sender
+    var receiverRoom: String? = null
+    var senderRoom: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +29,31 @@ class ChatActivity : AppCompatActivity() {
 
 
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
 
         supportActionBar?.title = name
         chatRecyclerView = findViewById(R.id.chatRecyclerview)
         messageBox = findViewById(R.id.messageBox)
         sendButton = findViewById(R.id.sentButton)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+
+        // adding the message to database
+        sendButton.setOnClickListener{
+            val message = messageBox.text.toString()
+            val messageObject = Message(message,senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+            messageBox.setText("")
+        }
     }
 }
