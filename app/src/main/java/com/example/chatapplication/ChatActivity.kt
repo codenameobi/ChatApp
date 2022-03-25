411b0319ffd3e8b26594cce880b5e4d929e0d12d
admin <tobynwude@gmail.com>
2022-03-25 07:58:52 +0100

    implement receive and sent messages in recyclerview

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -2,10 +2,21 @@ package com.example.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerview)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sentButton)
     }
 }
--- app/src/main/java/com/example/chatapplication/UserAdapter.kt
+++ app/src/main/java/com/example/chatapplication/UserAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.chatapplication
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
